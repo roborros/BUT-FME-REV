439adf52b01de7234d10c13b783c64269b61d3b2
kutilmisak <58466108+kutilmisak@users.noreply.github.com>
2020-01-21 14:23:44 +0100

    Update main_advanced.c

--- 05_ukazkovy_projekt/REV_basic_2019.X/main_advanced.c
+++ 05_ukazkovy_projekt/REV_basic_2019.X/main_advanced.c
@@ -1,29 +1,26 @@
 
-#pragma config WDTEN = OFF
-#pragma config FOSC = INTIO7
-#pragma config MCLRE = EXTMCLR
-#pragma config FCMEN = ON
-
-
-
+// REV-Basic 2018
+#pragma config FOSC = HSMP          // Oscillator Selection bits (HS oscillator (medium power 4-16 MHz))
+#pragma config PLLCFG = ON          // 4X PLL Enable (Oscillator used directly)
+#pragma config PRICLKEN = ON        // Primary clock enable bit (Primary clock is always enabled)
+#pragma config WDTEN = OFF          // Watchdog Timer Enable bits (Watch dog timer is always disabled. SWDTEN has no effect.)
 
 #include <xc.h>
 #include <stdio.h>
-
+#include <stdint.h>
+#include <stdbool.h>
 #include "rev-basic.h"
 #include "lcd.h"
 
 
-
-
 void main(void) {
+    
     REV_init();
-    
-    
-    int count = 0;
-    char a = 'x';
+    bool flag = true;
+    char count = 0;
     char text[17];
-    int pot1,pot2;
+    int32_t pot1;
+    int16_t pot2;
     
     
     sprintf(text,"Mechlab je bozi!");
@@ -31,47 +28,30 @@ void main(void) {
     sprintf(text,"                ");
     
     /* main loop */ 
-    while(1){
+    while(true){
         
-        
-        if (count == 1){
-            REV_led(1,1);
-            LATBbits.LATB0 = 1;
-            printf("Pot1: %d %d (%c)\n",HIGH_BYTE(pot1),LOW_BYTE(pot1),a);   
-            
-        }else if(count == 2){
-            REV_led(1,0);
-        }else if(count == 5){
-            LATBbits.LATB0 = 0;
-        }else if(count == 100){
+        if (count == 100 && flag){
             count = 0;
+            pot1 = (3300*pot1)>>10;
+            printf("Pot1: %d [mV]\n", pot1);   
         }
         
-        REV_led(2,REV_btn(1)); // 2ms wait
-        REV_led(3,REV_btn(2)); // 2ms wait
+        REV_led(2,REV_btn(1));
+        REV_led(3,REV_btn(2)); 
         
         pot1 = REV_pot(1);
         pot2 = REV_pot(2);
         
-        sprintf(text,"Pot: %d  %d", pot1,pot2);
+        sprintf(text,"Pot2: %d",pot2);
         LCD_ShowString(2,text);
         
-        if (REV_btn(3)){
+        if (PORTAbits.RA3){
             LCD_Clear();
         }
         
-        if (REV_btn(4)){
-            sprintf(text,"Mechlab je bozi!");
-            LCD_ShowString(1,text);
-            sprintf(text,"                ");
-        }
-        
-        
-        
-        __delay_us(10000 - 8000 - 1800);
-        count++; // apx. 10ms counter
-        
+        count++;
     }
     
 }
+
 
